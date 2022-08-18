db7e9a2a495309003ecf206e3064a0d6a14e6a97
FellipeMira <fellipe.mira@unesp.br>
2022-08-18 11:41:24 -0300

    start

---
+++ neighborhood/rgee_clas.R
@@ -0,0 +1,42 @@
+library(rgee)
+library(tidyverse)
+
+
+guara.ee <- st_bbox(guara) %>% 
+  st_as_sfc() %>% 
+  sf_as_ee() #Converts it to an Earth Engine Object
+
+x <- ee$Image("LANDSAT/LC08/C02/T1_L2/") %>% ee$Image$getInfo()
+
+land8 <- 
+  ee$ImageCollection("LANDSAT/LC08/C02/T1_L2")$
+  filter(ee$Filter$date('2014-01-01', '2022-12-31'))$
+  filterBounds(guara.ee)
+
+
+
+addNDVI <- function(image) {
+  
+  return(image$addBands(image$normalizedDifference(c("SR_B5", "SR_B4"))))
+}
+
+ndviCollection <- land8$map(addNDVI)
+lc8_gta<- ndviCollection
+class(lc8_gta)
+
+lc8_gta <- lc8_gta$select(c('ST_B10'))$toBands()  
+ee_print(lc8_gta)
+
+colorizedVis <- list(
+  min=10,
+  max=30,
+  palette=c(
+    'FFFFFF', 'CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901',
+    '66A000', '529400', '3E8601', '207401', '056201', '004C00', '023B01',
+    '012E01', '011D01', '011301'
+  )
+)
+
+b10 <- lc8_gta$select('LC08_218076_20140201_ST_B10')
+Map$addLayer(b10, colorizedVis, '2021 03 15')
+
