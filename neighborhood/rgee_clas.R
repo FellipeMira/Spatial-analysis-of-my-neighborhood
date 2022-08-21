171e6ab7186326b7447dc0c03c51409090773899
FellipeMira <fellipe.mira@unesp.br>
2022-08-21 12:28:45 -0300

    start

--- neighborhood/rgee_clas.R
+++ neighborhood/rgee_clas.R
@@ -1,33 +1,71 @@
 library(rgee)
 library(tidyverse)
 library(rgee)
+library(sf)
 
 #rgee::ee_install_upgrade()
 
 #reticulate::py_install('earthengine-api==0.1.317', envname='vrgee')
-
+  
 ee_Initialize(drive = T)
 
-guara.ee <- st_bbox(guara) %>% 
+guara.ee <- 
+  st_bbox(guara) %>% 
   st_as_sfc() %>% 
   sf_as_ee() #Converts it to an Earth Engine Object
 
-
+#Landsat 8 col
 land8 <- 
-  ee$ImageCollection("LANDSAT/LC08/C02/T1_L2")$
-  filter(ee$Filter$date('2014-01-01', '2022-12-31'))$
-  filterBounds(guara.ee)
+  ee$ImageCollection("LANDSAT/LC08/C02/T1_L2")
+#JRC col
+gsw <- ee$Image("JRC/GSW1_2/GlobalSurfaceWater")
 
+# cloud cover
+cc <- 60L
 
-
-addNDVI <- function(image) {
-  
-  return(image$addBands(image$normalizedDifference(c("SR_B5", "SR_B4"))))
+#l8 evi
+AddEVI <- function (img) {
+  evi <- img$expression(
+    expression = '2.5 * ((nir - red) / (nir + 2.4 * red + 1))', opt_map = list(
+      nir = img$select("B5")$multiply(0.0001),
+      red = img$select("B4")$multiply(0.0001)))
+  return (img$addBands(evi))
 }
 
-ndviCollection <- land8$map(addNDVI)
-lc8_gta<- ndviCollection
-class(lc8_gta)
+# Cloud mask
+maskClouds <- function(img) {
+  clear <- img$select('pixel_qa')$bitwiseAnd(2)$neq(0)
+  clear <- img$updateMask(clear)
+  return(clear)
+}
+
+# Water Mask
+water_mask <- gsw$select('seasonality')$lt(11)$unmask(1)$clip(guara.ee)
+
+#Clip por AOI
+cliplim <- function (img) {return (img$clip(guara.ee))}
+
+coleccion <- land8$
+  filterBounds(guara.ee)$
+  filterDate('2014-01-01', '2022-12-31')$
+  filterMetadata('CLOUD_COVER','less_than', cc)$
+  map(maskClouds)$
+  map(cliplim)
+
+
+#Aplicar función para EVI
+col1 <- coleccion$map(AddEVI)
+
+
+#Aplicar función para percentil y función de máscara de agua
+col1 <- col1$reduce(ee$Reducer$percentile(list(90)))$mask(water_mask)
+
+# Band names ver.
+bandNames <- col1$bandNames()
+ee_help(bandNames)
+rm(bandNames)
+
+clear_collection <- land8$map(maskClouds)
 
 lc8_gta <- lc8_gta$select(c('ST_B10'))$toBands()  
 ee_print(lc8_gta)
