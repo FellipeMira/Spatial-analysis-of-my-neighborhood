a5fabfa1cbee18229e6ade5aab26aac31f1201f1
FellipeMira <fellipe.mira@unesp.br>
2022-08-21 19:55:40 -0300

    start

--- neighborhood/hydro_gta.R
+++ neighborhood/hydro_gta.R
@@ -30,5 +30,31 @@ data_flu_acre <- stationsData(
   inventoryResult = inv_flu_acre,
   deleteNAstations = F)
 
+data_flu_acre$`2245192`
+
 tidy_data_acre <- organize(stationsDataResult = data_flu_acre)
+
+flu_acre <- selectStations(
+  # Pass on output from organize() function
+  organizeResult = tidy_data_acre,
+  # Decide by either yearly or monthly filter
+  mode = "yearly",
+  # Filter years with a maximum % of missing data
+  maxMissing = 10,
+  # Filter stations with a minimum of years available
+  minYears = 15,
+  # use civil year (month = 1) or define month for water year
+  month = 1,
+  # filter from initial to final years (or NULL to use entire time series)
+  iniYear = 1980,
+  finYear = 2020,
+  # Use only consisted or raw data as well?
+  consistedOnly = FALSE,
+  # Plot figure? TRUE by default
+  plot = TRUE
+)
+
+flu_acre
+
+tidy_data_acre <- bind_rows(tidy_data_acre)
 
